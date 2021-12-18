8ca33ed54a451f5fcc2f51cdeb146ea511e2fe12
Pete Whiting <pete@whitings.org>
2021-12-18 15:18:15 -0700

    added command line option for port

--- README.md
+++ README.md
@@ -2,4 +2,7 @@ Build:
 mvn package 
 
 Run:
-java -jar target/pwnme.jar
+java -jar target/pwnme.jar <port>
+
+default port is 1820
+consider running this as a user with few privledges
--- src/main/java/in/dugg/pwnme/LogMyAgent.java
+++ src/main/java/in/dugg/pwnme/LogMyAgent.java
@@ -5,11 +5,13 @@ import static spark.Spark.*;
 public class LogMyAgent {
     static final Logger logger = LogManager.getLogger(LogMyAgent.class.getName());
     public static void main(String[] args) {
+        int port = 1820;
+        if ( args.length > 0 ) { port = Integer.parseInt(args[ 0 ]); } 
 	System.setProperty("com.sun.jndi.ldap.object.trustURLCodebase","true");
 	System.setProperty("com.sun.jndi.rmi.object.trustURLCodebase","true"); 
 	System.setProperty("com.sun.jndi.cosnaming.object.trustURLCodebase","true");
 	System.setProperty("log4j2.formatMsgNoLookups","false");
-        port(1820);
+        port(port);
         get("/*", (req, res) -> {
             String agent = req.headers("User-Agent");
             System.out.println("User-Agent: " + agent);
